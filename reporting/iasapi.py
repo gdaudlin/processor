752d0ef3e61472916a6136feb731850ae0e63794
gdaudlin <gdaudlin@liquidarcade.com>
2025-10-27 11:57:44 -0400

    iasapi.py bug fixes -remove popup -check for missing start date

--- reporting/iasapi.py
+++ reporting/iasapi.py
@@ -24,7 +24,7 @@ class IasApi(object):
 
     dimensions = ['Teams', 'Campaigns', 'Media partners', 'Placements', 'Date']
 
-    def __init__(self, headless=True):
+    def __init__(self, headless=False):
         self.headless = headless
         self.sw = None
         self.browser = None
