055724991400004f98e3e98e75bfab3463ddcc8f
gdaudlin <gdaudlin@liquidarcade.com>
2026-01-21 09:53:49 -0500

    add option to change dashs to underscores in full placeement creation

--- reporting/vendormatrix.py
+++ reporting/vendormatrix.py
@@ -612,13 +612,21 @@ class ImportConfig(object):
 def full_placement_creation(df, key, full_col, full_place_cols):
     logging.debug('Creating Full Placement Name')
     df[full_col] = ''
-    df = utl.data_to_type(df, str_col=[x[2:] if x[:2] == '::' else x
-                                       for x in full_place_cols])
+    df = utl.data_to_type(
+        df, str_col=[x[2:] if x.startswith(('::', '--'))
+                     else x for x in full_place_cols])
     for idx, col in enumerate(full_place_cols):
         if col[:2] == '::':
             col = col[2:]
             if col in df.columns:
                 df[col] = df[col].str.replace('_', '', regex=True)
+        if col[:2] == '--':
+            col = col[2:]
+            if col in df.columns:
+                campaign_name_check = ~df[col].str.match(r'(?i)^pre-launch$')
+                df.loc[campaign_name_check, col] = df.loc[
+                    campaign_name_check, col].str.replace(
+                    ' - ', '_', regex=True).replace('-', '_', regex=True)
         if col not in df:
             msg = '{} was not in {}.  It was not included in {}'.format(
                 col, key, dctc.FPN)
